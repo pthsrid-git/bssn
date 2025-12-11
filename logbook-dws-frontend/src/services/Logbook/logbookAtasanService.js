5bdb559ba28923faab5831c7994d34e740a737f1
Andreyap27 <67877083+Andreyap27@users.noreply.github.com>
2025-12-11 17:23:12 +0700

    add config for build vue

--- logbook-dws-frontend/src/services/Logbook/logbookAtasanService.js
+++ logbook-dws-frontend/src/services/Logbook/logbookAtasanService.js
@@ -1,7 +1,7 @@
 // services/Logbook/logbookAtasanService.js
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
   baseURL: API_URL,
